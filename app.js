9d0a952e8e5ced98f9eee8cf261320a8e9963753
Ryan SIU <ryansiu1995@gmail.com>
2020-12-26 22:12:11 +0800

    Add the random function

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 "use strict";
 
+window.number = Math.floor(Math.random() * 100);
+
 function speak() {
   if (typeof speechSynthesis === 'undefined') {
     console.error("There is no speech synthesis API in this browser");
   } else {
-    var utterThis = new SpeechSynthesisUtterance("88");
+    const utterThis = new SpeechSynthesisUtterance(window.number);
     utterThis.lang = "fr";
     speechSynthesis.speak(utterThis);
   }
+}
+
+function random() {
+  window.number = Math.floor(Math.random() * 100);
 }
